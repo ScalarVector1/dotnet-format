d32fff68940217db7797df32d105e2bc883aed98
ScalarVector <bobfishkins23@yahoo.com>
2023-03-18 00:51:49 -0400

    Update dotnet.ts

--- src/dotnet.ts
+++ src/dotnet.ts
@@ -37,7 +37,7 @@ function formatOnlyChangedFiles(onlyChangedFiles: boolean): boolean {
 async function formatVersion3(options: FormatOptions): Promise<boolean> {
   const execOptions: ExecOptions = { ignoreReturnCode: true };
 
-  const dotnetFormatOptions = ["format", "--check"];
+  const dotnetFormatOptions = ["format", "style", "--check"];
 
   if (options.dryRun) {
     dotnetFormatOptions.push("--dry-run");
